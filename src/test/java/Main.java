b85e76c264b21a3b77889fd2dcb75d6eacdc1e18
Adeliya <89214241+AAdeliya@users.noreply.github.com>
2023-11-29 17:57:57 -0800

    Hello World

---
+++ src/test/java/Main.java
@@ -0,0 +1,14 @@
+public class Main { // class name , public means that any other class can access it
+
+public static void main(String[] args){  //this is entry point of Java program
+    //static means that you can run this method without creating an instance of Main
+    //void means that this method doesn't return any value
+    //main is the name of the method
+    System.out.println("Hello world");
+
+    //System is a pre-defined class that Java provides us and it holds some useful methods and variables
+    //out is a static variable within System that represents the output of your program
+    //println is a method of out that can be used to print a line
+
+}
+}
