451799f6dce50948022c0b95351f12cf666597dd
Adeliya <89214241+AAdeliya@users.noreply.github.com>
2023-12-10 18:19:13 -0800

    Abstraction

---
+++ src/test/java/Mmain.java
@@ -0,0 +1,7 @@
+public class Mmain {
+    public static void main(String[] args) {
+        Pig myPig = new Pig(); //Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
