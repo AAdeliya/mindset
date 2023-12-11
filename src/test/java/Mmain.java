f013fe74ba21551808b441d70f7672c6a481e37c
Adeliya <89214241+AAdeliya@users.noreply.github.com>
2023-12-11 13:21:13 -0800

    Abstraction

--- src/test/java/Mmain.java
+++ src/test/java/Mmain.java
@@ -1,7 +1,18 @@
 public class Mmain {
+  //  public static void main(String[] args) {
+       // Pig myPig = new Pig(); //Create a Pig object
+       // myPig.animalSound();
+       // myPig.sleep();
+   // }
+
     public static void main(String[] args) {
-        Pig myPig = new Pig(); //Create a Pig object
-        myPig.animalSound();
-        myPig.sleep();
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+
     }
+
+
 }
---
+++ src/test/java/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName()
+    { return name
+            ;
+
+    }
+    public int getAge(){ return age;}
+    public void setAge(int age) {this.age= age;}
+
+    public void setName(String john) {
+    }
+}
