838f7f2c9a9766e81c071b10611995a1d79c5ac8
Adeliya <89214241+AAdeliya@users.noreply.github.com>
2023-12-25 12:34:36 -0800

    HashMap.java

---
+++ src/test/java/HashMap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class HashMap1 {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washigton DC");
+        System.out.println(capitalCities);
+    }
+
+}
