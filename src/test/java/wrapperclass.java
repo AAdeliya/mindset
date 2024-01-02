5e929d2e92e354230654bfd469f80878cc92257a
Adeliya <89214241+AAdeliya@users.noreply.github.com>
2024-01-02 13:08:17 -0800

    wrapperclass

---
+++ src/test/java/wrapperclass.java
@@ -0,0 +1,15 @@
+public class wrapperclass {
+    public static void main(String[] args) {
+        Integer myInt=5;
+        Double mydouble= 5.99;
+        Character myChar= 'A';
+        System.out.println(myInt);
+        System.out.println(mydouble);
+        System.out.println(myChar);
+        Integer myInt2= 100;
+        String myString = myInt2.toString();
+        System.out.println(myString.length());
+
+
+    }
+}
