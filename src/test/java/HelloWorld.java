1e59c9ab80281a0a33a2f0b167d095caca6ed528
firstPush <adel.sdet2@gmail.com>
2024-04-25 17:50:57 -0500

    getters and setters

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         Person johnny = new Person("Johnny", "Smith", 30);
         //johnny is variable
         Person amy = new Person(); //new instance of the Person
+        System.out.format("The person's first name is: %s. ", johnny.getFirstName());
     }
     
 }
