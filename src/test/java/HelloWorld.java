bdfba237e438d72995981916346ec029e8671f0c
firstPush <adel.sdet2@gmail.com>
2024-04-25 20:41:20 -0500

    push

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        Person johnny = new Person("johnny", "Smith", 30);
+        Person johnny = new Person("johnny", "smith", 30);
         //johnny is variable
         Person amy = new Person(); //new instance of the Person
         System.out.format("The person's first name is: %s. ", johnny.getFirstName());
+
+        //johnny.setLastName("smith");
+       // System.out.printf("This is last name: %s." , johnny.getFirstName());
+       
+        
     }
     
 }
--- src/test/java/Person.java
+++ src/test/java/Person.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+
 public class Person {
     private String firstname;
     private int age;
     private String name;
     private String lastname;
+    
 
     public Person(){
         //no arg constructor, does not take any inputs
@@ -12,6 +15,8 @@ public class Person {
         this.firstname= firstname;
         this.lastname= lastname;
         this.age= age;
+      
+
 
         //you can pass a value for firstname, lastname, age
 
@@ -22,9 +27,21 @@ public class Person {
         return firstname.substring(0, 1).toUpperCase() + firstname.substring(1);
 
     }
+    public void setLastName(String lastname){
+        this.lastname = lastname.substring(0, 1).toUpperCase() + lastname.substring(1);
+    }
+    // public String getlastname()
+    // { 
+    //     return lastname;
+    // }
     public int getAge(){ return age;}
     public void setAge(int age) {this.age= age;}
 
     public void setName(String john) {
     }
-}
+    // public String setLastName(){
+    //     return lastname;
+    }
+    
+//}
+//getters and setter help us to protect data
