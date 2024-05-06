c034102ea9b6aecfbe6d76f9d7d096cd4baf7f8f
firstPush <adel.sdet2@gmail.com>
2024-05-05 19:37:23 -0500

    bytearray

---
+++ src/test/java/ByteArrayInputStream.java
@@ -0,0 +1,30 @@
+
+// import java.io.*;
+// public class ByteArrayInputStream {
+//     public ByteArrayInputStream(byte[] buf) {
+//         //TODO Auto-generated constructor stub
+//     }
+
+//     public static void main(String[] args) {
+    
+//     {
+ 
+//         // Create byte array
+//         byte[] buf = { 71, 69, 69, 75, 83 };
+ 
+//         // Create byteArrayInputStream
+//         ByteArrayInputStream byteArrayInputStr
+//             = new ByteArrayInputStream(buf);
+ 
+//         int b = 0;
+//         while ((b = byteArrayInputStr.read()) != -1) {
+//             // Convert byte to character
+//             char ch = (char)b;
+ 
+//             // Print the character
+//             System.out.println("Char : " + ch);
+//         }
+//     }
+// }
+// }
+    
