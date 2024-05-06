bfa5dccb42fd90b50bb7365868aa9a2613fd426e
firstPush <adel.sdet2@gmail.com>
2024-05-06 12:17:59 -0500

    regex

--- src/test/java/RegexPractice.java
+++ src/test/java/RegexPractice.java
@@ -1,6 +1,9 @@
 public class RegexPractice {
     public static void main(String[] args) {
-        System.out.println("cat".matches("[^c]at"));
+        //System.out.println("cat".matches("[^c]at"));
+         //\\w for letters 
+         //\\d for numbers 
+        System.out.println("321.333.7652".matches("\\d{3}[-.]\\d{3}[-.]\\{4}"));
     }
     
 }
