d61d5a7517bd34044fc6fe5839ba0140cad39d2b
firstPush <adel.sdet2@gmail.com>
2024-05-09 16:56:02 -0400

    regex

--- src/test/java/RegexPractice.java
+++ src/test/java/RegexPractice.java
@@ -1,11 +1,34 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexPractice {
     public static void main(String[] args) {
         //System.out.println("cat".matches("[^c]at"));
          //\\w for letters 
          //\\d for numbers 
-        System.out.println("321.333.7652".matches("\\d{3}[-.]\\d{3}[-.]\\{4}"));
+         String regex= "(1[-.,\\s]?)?(\\d{3}[-.,\\s]?)(\\d{3}[-.,\\s]?)\\d{4}";
+
+         //{1,2}--qualifiers, how many times this patttern should be applied
+         String phoneNumber="1.232.333.2365";
+        System.out.println("232.333.2365".matches(regex));
+        Pattern pat = Pattern.compile(regex);
+        //expensive operation for CPU 
+        Matcher mat =pat.matcher(phoneNumber);
+        //does this phone number matches the pattern regex
+
+        if(mat.matches())
+        {
+          System.out.println(mat.group(1));
+          System.out.println(mat.group(2));
+          System.out.println(mat.group(3));
+          
+          //true
+          //null
+          //333.
+        }
+
     }
-    //
-    //
+
+    
     
 }
