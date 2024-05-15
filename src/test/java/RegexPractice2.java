73413a9d8c7e9499c0a4f0b352c1711ce79940c2
firstPush <adel.sdet2@gmail.com>
2024-05-14 21:08:04 -0500

    ooo

--- src/test/java/RegexPractice2.java
+++ src/test/java/RegexPractice2.java
@@ -1,8 +1,10 @@
 public class RegexPractice2 {
     public static void main(String[] args) {
-        System.out.println("doggy".matches(".?"));
+        System.out.println("doggy".matches("^.....$"));
         //. represent any character 
         //in this case ..... returns true
+        //^..... begining of the line 
+        //$ end of the string
     }
     
 }
