b8bb3f318019f6d9f3f4475c71348b1fdda361bf
firstPush <adel.sdet2@gmail.com>
2024-05-14 21:01:42 -0500

    practice

---
+++ src/test/java/RegexPractice2.java
@@ -0,0 +1,8 @@
+public class RegexPractice2 {
+    public static void main(String[] args) {
+        System.out.println("doggy".matches(".?"));
+        //. represent any character 
+        //in this case ..... returns true
+    }
+    
+}
