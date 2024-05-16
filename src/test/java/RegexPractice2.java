9fb9bb4490285e55a76b2a413e0dc5245486034a
firstPush <adel.sdet2@gmail.com>
2024-05-15 21:37:53 -0500

    chan

--- src/test/java/RegexPractice2.java
+++ src/test/java/RegexPractice2.java
@@ -1,10 +1,15 @@
 public class RegexPractice2 {
     public static void main(String[] args) {
-        System.out.println("doggy".matches("^.....$"));
+        System.out.println("cat doggy".matches("...\\s\\b.....$"));
         //. represent any character 
         //in this case ..... returns true
         //^..... begining of the line 
         //$ end of the string
+        //\\b boundary 
+        //\\s space 
+        //\\W not words characaters letters number and under scores 
+        //\\D not digits
+        
     }
     
 }
---
+++ src/test/java/TranscriptParser.java
@@ -0,0 +1,7 @@
+public class TranscriptParser {
+    public static void main(String[] args) {
+        //
+           
+    }
+    
+}
