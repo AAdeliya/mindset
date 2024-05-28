042f6f36f51e3dae209fbd6fed211f9eade4e62e
firstPush <adel.sdet2@gmail.com>
2024-05-27 21:49:25 -0500

    math

--- src/test/java/MathStuff.java
+++ src/test/java/MathStuff.java
@@ -1,6 +1,13 @@
 public class MathStuff {
     public static void main(String[] args) {
-        System.out.println(7*3);
+       // System.out.println(7/4); //will be int
+      // System.out.println(7f/4); //1.75 float
+       int num1=7;
+       int num2=4;
+
+      System.out.println(num1+num2);
+      
+
     }
     
 }
