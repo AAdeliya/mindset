42cbedbe6b2270208a8c81b8b8f5864f1ee5f6b5
firstPush <adel.sdet2@gmail.com>
2024-05-28 14:44:47 -0500

    math

---
+++ src/test/java/Math.java
@@ -0,0 +1,15 @@
+public class Math {
+    public static void main(String[] args) {
+        //System.out.println(Math.ceil(5.0)); //round up to integer celling
+        //floor -->
+        //Math.min(num1, num2) -->3 output
+        //int num1=3, num2=5
+        //max -->5
+        //pow
+        //random -->
+        //sqrt-->root
+        //Math.PI
+
+    }
+    
+}
