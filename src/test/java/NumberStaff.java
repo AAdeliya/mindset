7d262e182a41fb7b0e9b3d36347bd3c638a7fbab
firstPush <adel.sdet2@gmail.com>
2024-05-29 14:04:13 -0500

    math

--- src/test/java/NumberStaff.java
+++ src/test/java/NumberStaff.java
@@ -1,4 +1,6 @@
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class NumberStaff {
     public static void main(String[] args) {
@@ -8,6 +10,9 @@ public class NumberStaff {
         System.out.println(num1-num2);
         BigDecimal num4 = new BigDecimal(1.10);
         System.out.println(num3.subtract(num4));
+
+        System.out.println(new BigDecimal(.3134566).add(new BigDecimal(5.321)));
+        MathContext mc= new MathContext(5, RoundingMode.HALF_DOWN);
     }
     
 }
