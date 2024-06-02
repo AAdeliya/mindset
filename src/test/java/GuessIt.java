3641b9064c0103898e6f7747afcf9ac65533d582
firstPush <adel.sdet2@gmail.com>
2024-06-01 21:39:19 -0500

    commit

---
+++ src/test/java/GuessIt.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class GuessIt {
+    public static void main(String[] args) {
+        int num = new Random().nextInt(3); //up but not include 
+        
+        //
+        
+    }
+    
+}
