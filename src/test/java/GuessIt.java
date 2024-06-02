65dce94bf5e54f59a04a93527b63ea0dc960c550
firstPush <adel.sdet2@gmail.com>
2024-06-02 17:14:38 -0500

    if

--- src/test/java/GuessIt.java
+++ src/test/java/GuessIt.java
@@ -2,10 +2,27 @@ import java.util.Random;
 
 public class GuessIt {
     public static void main(String[] args) {
-        int num = new Random().nextInt(3); //up but not include 
+        int randomNum = new Random().nextInt(5)+1; //up but not include 
+        System.out.printf("Generated number is: %d.%n", randomNum);
         
-        //
+        if(randomNum ==1){
+            //then
+            System.out.println(" The color is RED");
+        }else if(randomNum ==2){
+            System.out.println("The color is GREEN");
         
-    }
+        }else if(randomNum==3) { //if is false
+            System.out.println("The color is BLUE");
+
+        }else{
+            System.out.println("The color is POLKA-DOT");
+        }
+        }
+        // private static boolean shouldIGo(){
+        //     return false;
+
+        }
+
     
-}
+    
+
