3313df7b30ce93731d05bc9b62298ed22f684aac
firstPush <adel.sdet2@gmail.com>
2024-06-03 12:35:40 -0500

    switch

--- src/test/java/GuessIt.java
+++ src/test/java/GuessIt.java
@@ -5,22 +5,41 @@ public class GuessIt {
         int randomNum = new Random().nextInt(5)+1; //up but not include 
         System.out.printf("Generated number is: %d.%n", randomNum);
         
-        if(randomNum <=2){ //1 2 3 4 5 6 7 8
-            //then
-            System.out.println(" The color is RED");
-        }else if(randomNum >=4){
-            System.out.println("The color is BLUE");
+        // if(randomNum <2){ //1 2 3 4 5 6 7 8
+        //     //then
+        //     System.out.println(" The color is RED");
+        // }else if(randomNum >4&& (randomNum%2 ==0)){
+        //     System.out.println("The color is BLUE");
         
-        }else if(randomNum==3) { //if is false
-            System.out.println("The color is GREEN");
+        // }else  { //if is false
+        //     System.out.println("The color is GREEN");
 
       
-        }
-        // private static boolean shouldIGo(){
-        //     return false;
+        // }
+        // // private static boolean shouldIGo(){
+        // //     return false;
 
-        }
-    }
+        // }
+        switch (randomNum){
+            case 1:
+            System.out.println("THe color is RED");
+            break;
+            case 2:
+            System.out.println("The color is BLUE");
+            break;
+            case 3:
+            System.out.println("The color is GREEN");
+            break;
+            case 4:
+            System.out.println("THe color is PURPLE");
+            break;
+            default:
+            System.out.println("The color is WHATEVER");
+
+
+
+
+    }}}
     
     
 
