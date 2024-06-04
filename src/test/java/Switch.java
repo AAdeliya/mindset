cae0cad9ea68135295cbec5f6e59cf80f0af9dd2
firstPush <adel.sdet2@gmail.com>
2024-06-04 08:36:24 -0500

    go

---
+++ src/test/java/Switch.java
@@ -0,0 +1,13 @@
+public class Switch {
+    public static void main(String[] args) {
+        String command= null;
+        if(command.equals("stop")){
+            System.out.println("Stopping now...");
+
+
+        }else if(command.equals("go")){
+            System.out.println("Going now...");
+        }
+    }
+    
+}
