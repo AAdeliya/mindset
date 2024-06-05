286ea295b2051e9e992639c99802e0790620afa1
firstPush <adel.sdet2@gmail.com>
2024-06-05 12:14:20 -0500

    loop

---
+++ src/test/java/Loop.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Loop {
+    public static void main(String[] args) {
+        int rabdomNum= new Random().nextInt(10) +1;
+        String gussedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively");
+        int gussedNum = Integer.parseInt(gussedNumText);
+        if(gussedNum == rabdomNum){
+            System.out.printf("The random number was %d. You got it !%n", rabdomNum);
+
+
+        }else{
+            System.out.printf("The random number was %d. You didn't get it!", rabdomNum);
+            
+        }
+
+        
+    }
+    
+}
