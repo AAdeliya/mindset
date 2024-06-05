4165271e2c693e3db0bfa7e180894d1adb953c09
firstPush <adel.sdet2@gmail.com>
2024-06-05 12:01:46 -0500

    switch

---
+++ src/test/java/Cards.java
@@ -0,0 +1,26 @@
+public class Cards {
+    public static void main(String[] args) {
+        String card = "jack";
+        int currentTotalValue  =10;
+
+        //int currentTotalValue = switch(card){
+            // case "king", "quenn", "jack" -> 10;  not supported
+            //case "ace" -> {
+                //if(currentTotalValue<11){
+                    //yield 11;
+
+
+
+               // }else{
+               // yield 1;
+
+
+              // }
+
+            //}
+            // default -> Integer.parseInt(card); 
+
+        };
+    }
+    
+//}
