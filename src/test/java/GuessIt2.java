7d8d0794293568371c2d6ddfced0d1c68b778704
firstPush <adel.sdet2@gmail.com>
2024-06-11 15:17:16 -0500

    lop

--- src/test/java/GuessIt2.java
+++ src/test/java/GuessIt2.java
@@ -5,20 +5,20 @@ public class GuessIt2 {
         int random = new Random().nextInt(10) +1;
         System.out.printf("Random number is %d.%n", random);
        
-        String gussedNumbertext= null;
-        int wrongGuessCount =0;
-
-
-
+        String gussedNumbertext= null; 
+        
+        int wrongGuessCount =1;
         do{//execute at least one
-
-
            gussedNumbertext = System.console().readLine(" PLease guess number between 1 to 11");
             if(gussedNumbertext.matches("-?\\d{1,2}")){
             //increasing complexity of the program by adding nested loop
             int gussedNum = Integer.parseInt(gussedNumbertext);
             if(gussedNum== random){
-                System.out.printf("The random number was %d. You got it in %d of tries!%n", random, wrongGuessCount);
+                String tryText = wrongGuessCount ==1 ? "try" : "tries";
+                
+                
+                
+                System.out.printf("The random number was %d. You got it in %d %s!%n", random, wrongGuessCount, tryText);
                 return;
             }else{
                 wrongGuessCount++;
