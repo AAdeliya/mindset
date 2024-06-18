f7edb7cb8a8b3bc584509b1400cab6bf083bb354
firstPush <adel.sdet2@gmail.com>
2024-06-18 11:02:41 -0500

    semantic

---
+++ src/test/java/SemanticAnalysis.java
@@ -0,0 +1,40 @@
+public class SemanticAnalysis {
+//enforcing constrains
+    //public class Test{
+    // final int MAX_VALUE = 100;
+    // void updatedValue(){
+    //     MAX_VALUE= 200;
+
+    // }
+    
+ //   } 
+
+ //type checking
+ //int a=5
+ //double b= 2.14
+ //int c = a+b
+
+
+ //logical error detection
+ //main
+ //int [] numbers = {1,2,3,4,5};
+ //int index = 5;
+ //int value = number[index];
+ //sout ("Value at index "  + index + " is " + value)
+
+
+ //scope resolution
+ //{
+  //int a = 10;
+  //{
+  //a=20;
+  //int b=30;
+  //}
+  //b=40;
+  //}
+
+
+
+
+    
+}
