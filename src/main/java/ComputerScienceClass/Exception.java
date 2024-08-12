2e1027ac8724da6e3d6d34b45c9983e7042dc908
firstPush <adel.sdet2@gmail.com>
2024-08-12 16:47:17 -0500

    cc

---
+++ src/main/java/ComputerScienceClass/Exception.java
@@ -0,0 +1,25 @@
+package ComputerScienceClass;
+
+public class Exception {
+    public static void main(String[] args) {
+        int a =1;
+        int b = 0;
+
+        //try block to check for exceptions
+
+       try{
+        int i = computeDivision(a,b);
+       } 
+       catch (ArithmeticException ex) {
+        System.out.println(ex.getMessage());
+        
+       }
+
+    }
+
+    private static int computeDivision(int a, int b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'computeDivision'");
+    }
+    
+}
