76c2362d99f674a2dc19f81b72c9a46cbed7a381
firstPush <adel.sdet2@gmail.com>
2024-09-04 13:52:03 -0500

    cc

--- src/main/java/ComputerScienceClass/Main.java
+++ src/main/java/ComputerScienceClass/Main.java
@@ -2,8 +2,20 @@ package ComputerScienceClass;
 
 public class Main {
     public static void main(String[] args) {
-        Dog Peter= new Dog();
-        Peter.eat();
+        // Dog Peter= new Dog();
+        // Peter.eat();
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <=5; i++) {
+            printLine("one more");
+            System.out.println("i = " + i);
+            System.out.println("more");
+
+        }
     }
-    
+
+    public static void printLine(String line) {
+        System.out.println(line); 
+        
+    }
 }
